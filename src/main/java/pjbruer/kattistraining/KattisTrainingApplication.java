607f2f6fe4da606f6ac84961752e8852b2c029d4
Jacob Bruér <bruer.pj@gmail.com>
2020-09-28 09:05:47 +0200

    code and file cleanup

--- src/main/java/pjbruer/kattistraining/KattisTrainingApplication.java
+++ src/main/java/pjbruer/kattistraining/KattisTrainingApplication.java
@@ -1,13 +1,8 @@
 package pjbruer.kattistraining;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class KattisTrainingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KattisTrainingApplication.class, args);
 		System.out.println("Time to katta");
 	}
 }
