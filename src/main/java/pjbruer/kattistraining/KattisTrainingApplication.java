ee56e30006c1dfd2739d8117e80a9c6d8236f5f6
Jacob Bruér <bruer.pj@gmail.com>
2020-09-28 08:25:14 +0200

    first assignment baseplate

--- src/main/java/pjbruer/kattistraining/KattisTrainingApplication.java
+++ src/main/java/pjbruer/kattistraining/KattisTrainingApplication.java
@@ -3,11 +3,19 @@ package pjbruer.kattistraining;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.*;
+
 @SpringBootApplication
 public class KattisTrainingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KattisTrainingApplication.class, args);
-		System.out.println("Time to katta");
+
+		final String text = "THE RAIN IN SPAIN";
+		final String text2 = "IN THE RAIN AND THE SNOW";
+		final String text3 = "THE RAIN IN SPAIN IN THE PLAIN";
+
+		Scanner sc = new Scanner(System.in);
+		//final String sentence = sc.nextLine();
 	}
 }
